759928e99da3d528c7c252446acb50e133efc96e
divyag2196 <goruledivya2196@gmail.com>
2025-12-16 13:04:04 +0530

    Initial Datacharya coming soon page

---
+++ src/components/ComingSoon/ComingSoon.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./ComingSoon.scss";
+
+function ComingSoon() {
+  return (
+    <div className="coming-wrapper">
+      <div className='container'>
+        <div className="row vh-100 justify-content-center align-items-center">
+          <div className="col-12 text-center">
+            <h1 className="main-heading">
+              Indian wisdom applied to
+              <br />
+              Global business problems
+            </h1>
+            <p className='coming-text'>coming soon...</p>
+            <div className="contact-text">
+              Get in touch
+              <br />
+              balaji.aravamuthan@datacharya.ai
+            </div>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+  )
+}
+
+export default ComingSoon
