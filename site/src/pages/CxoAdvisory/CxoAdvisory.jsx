43391a1ea2cd40f5864e281e0e177ff266dc863b
HuestormGit <sameer@huestorm.com>
2026-03-23 15:23:27 +0530

    create site folder
    try to upload all pagaes in / site folder

---
+++ site/src/pages/CxoAdvisory/CxoAdvisory.jsx
@@ -0,0 +1,136 @@
+import { Link } from "react-router-dom";
+import "./CxoAdvisory.scss";
+function CxoAdvisory() {
+  return (
+    <section id="CxoAdvisory">
+        
+        <div className="container header-part text-center">
+            <div className="hero">
+                <h1 className="Data-200-64-64">Empowering leadership for sustainable growth</h1>
+                <p className="sub-head-22-30">Strategic Insights for CXOs - Empowering Leaders for Sustainable Growth in a Dynamic Landscape</p>
+            </div>
+
+            <div className="hero2">
+                <h2 className="Data-300-64-64">CXO Advisory</h2>
+                <p className="body-16-24">In today's dynamic landscape, CXOs must leverage IT, AI, and shared services to drive operational excellence. This guide offers actionable strategies for navigating modern challenges, integrating shared services, finance, and HR to bridge data gaps, optimize processes, and drive sustainable growth. Equip CXOs with strategic foresight and innovation frameworks to navigate disruptions and build a culture of adaptability and resilience.</p>
+            </div>
+
+        </div>
+
+        <div className="my-fluid-container bgbox">
+            <div className="information">
+                <div className="main_box">
+                    <div className="heading">
+                        <h3 className="Data-700-24-32">KEY CHALLENGES WE ADDRESS</h3>
+
+                    </div>
+                    <div className="info_box">
+                        <div className="card">
+                            <p className="body-16-24">Bridging Data Silos and Ensuring Data Quality</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Enhancing Cross-Functional Collaboration Through Shared Services</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Addressing Inefficiencies in IT and Operational Processes</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Aligning Finance, HR, and ESG with Strategic Objectives</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="main_box">
+                    <div className="heading">
+                        <h3 className="Data-700-24-32">ACTIONABLE STRATEGIES AND FRAMEWORKS</h3>
+
+                    </div>
+                    <div className="info_box">
+                        <div className="card">
+                            <p className="body-16-24">The Resilience Framework</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Leadership Transformation Blueprint</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">The Unified Data Strategy Framework</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Key Outcomes for CXOs</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="main_box">
+                    <div className="heading">
+                        <h3 className="Data-700-24-32">STRATEGIC PILLARS FOR CXO SUCCESS</h3>
+
+                    </div>
+                    <div className="info_box">
+                        <div className="card">
+                            <p className="body-16-24">Cost Leadership</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Integrated Financial Strategies</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Operational Efficiency</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Shared Services for Synergy</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">IT and Digital Transformation</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Unified Digital Frameworks</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">AI and Automation at Scale</p>
+                        </div>
+                        <div className="card">
+                            <p className="body-16-24">Cybersecurity Excellence</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="main_box1">
+                    <div className="heading">
+                        
+
+                    </div>
+                    <div className="info_box1">
+                        <p className="sub-head-22-30">Assess your organizational readiness, set measurable goals, and execute with discipline. The opportunity to lead transformative change is now.</p>
+
+                        <Link to="/contact">
+                <button className="elementor-btn-shadow body-16-24">Get in touch</button>
+                </Link>
+                        
+
+                    </div>
+
+                </div>
+            
+
+
+            </div>
+
+        </div>
+
+        
+
+        <div className="spacer"></div>
+       
+
+    </section>
+    
+  )
+}
+
+export default CxoAdvisory
