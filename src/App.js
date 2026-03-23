b38d30404bdeeb58e7a555a6c188d43b4201ce7b
HuestormGit <sameer@huestorm.com>
2026-03-23 17:20:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import ComingSoon from "./components/ComingSoon/ComingSoon";
 // import ComingSoon from "./components/comming/ComingSoon";
+import Contact from "./pages/Contact/Contact";
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       
       <ComingSoon />
+      <Contact />
       
     </div>
   );
