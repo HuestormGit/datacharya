4731a7bb08b8c15d5512c9e46b2b41a534904c5e
HuestormGit <sameer@huestorm.com>
2026-03-23 17:35:57 +0530

    Update App.js
    add  url for other pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 // import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ComingSoon from "./components/ComingSoon/ComingSoon";
 // import ComingSoon from "./components/comming/ComingSoon";
+import About from "./pages/About/About"
+import Platforms from "./pages/Platforms/Platforms"
+import Blogs from "./pages/Blogs/Blogs"
+import CxoAdvisory from "./pages/CxoAdvisory/CxoAdvisory";
+import Approach from "./pages/Approach/Approach"
+
 // import Contact from "./pages/Contact/Contact";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
-      <ComingSoon />
+      // <ComingSoon />
       {/* <Contact /> */}
+    <BrowserRouter>
+    <Routes>
+
+  {/* <Route path="/" element={<Home />} /> */}
+    <Route path="/" element={<ComingSoon />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/approach" element={<Approach />} />
+        
+        <Route path="/platforms" element={<Platforms />} />
+
+        <Route path="/advisory" element={<CxoAdvisory />} />
+
+        <Route path="/blogs" element={<Blogs />} />
+
+{/* <Route path="/contact" element={<Contact />} /> */}
+
+      </Routes>
+    </BrowserRouter>
       
     </div>
   );
