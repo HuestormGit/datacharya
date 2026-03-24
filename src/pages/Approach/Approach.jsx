698a0d6c5f5c944b4ffdc851da9b593662d1ed44
HuestormGit <sameer@huestorm.com>
2026-03-24 18:33:09 +0530

    comment link

--- src/pages/Approach/Approach.jsx
+++ src/pages/Approach/Approach.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import SENSE from "../../assets/sense.png";
 import ALIGN from "../../assets/align.png";
 import GOVERN from "../../assets/govern.png";
